78a4639c2bfd6a5ca7cfd0a38a5bce75b1da50ee
shin748 <shin748@naver.com>
2022-11-12 21:13:12 +0900

    a

--- dicom with optical flow.py
+++ dicom with optical flow.py
@@ -4,9 +4,11 @@ from excel import excel
 #excel().save_all_optflow_plot(): 엑셀의 모든 대상의 하강벡터를 저장 (약 10분 소요)
 
 def main():
-    excel().show_optflow_plot(24, preprocess=True, interpolate=True, opt=False)
+    excel().show_plot(21, preprocess=True, interpolate=True, cycle=2, opt=False)
+    #excel().save_all_plot(preprocess=True, interpolate=True, cycle=2, opt=False)
     #preprocess: 전처리 사용/미사용
     #interpolate: 피크 보간법 사용/미사용
+    #cycle: 보간 횟수(적정은 2회일듯)
     #opt: 옵티컬플로우 사용 / 밝기값 사용
     
 if __name__ == '__main__': main()
