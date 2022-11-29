67d3180f2a496455f4de5405c6ebd3f284b19180
shin748 <62280053+shin748@users.noreply.github.com>
2022-11-29 23:43:03 +0900

    Update excel.py

--- excel.py
+++ excel.py
@@ -5,19 +5,13 @@ import pandas as pd
 from optflow import dicom_optflow
 from intense_sum import dicom_intense
 from scipy import interpolate
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
-from sklearn.preprocessing import PolynomialFeatures
-import sympy as sp
-from sklearn.mixture import GaussianMixture
-from sklearn.cluster import KMeans
 from sklearn.model_selection import KFold, cross_val_score, cross_validate
 from interpolation import interp
 from data_analysis import analysis
 from sklearn.metrics import mean_squared_error
 from math import sqrt
-import sys
 
 
 #엑셀파일로 경로를 파악하기 위한 클래스 (+벡터수 저장/확인용 함수)
@@ -47,17 +41,24 @@ class excel:
         return paths
     #####################################################
 
-    def set_all_plot(self, save=False, bas=True, preprocess=False, interpolate=False, cycle=1, opt=False):
+    def set_all_plot(self, save=False, bas=True, preprocess=False, interpolate=False, opt=False):
         paths = self.load_excel()
         plen = len(paths)
+        results=[]
+        
+        for i in range(5): #원래는 5가 아니라 따로 변수로 지정해야함
+            results.append([]) # results = [ [2차피팅], [3차피팅], [4차피팅], [5차피팅], ...]
+            results[i].append([]); results[i].append([]); results[i].append([]); results[i].append([]) #[n차피팅] 안에 [ [TFCS들], [TFCE들], ...]
 
-        for i in range(plen):
-            tfcs_pred=[]; tfce_pred=[]; ccfcs_pred=[]; ccfce_pred=[]
+        for i in range(plen): #모든 파일들에 대해
             rtfcs=[]; rtfce=[]; rccfcs=[]; rccfce=[]
+            ptfcs=[]; ptfce=[]; pccfcs=[]; pccfce=[]
+
             dicom_data = pydicom.dcmread(paths[i]) #force=True 인자를 넣으면 인식안되는 dicom 읽을수 있음
             dicom_img = dicom_data.pixel_array #dicom이미지 호출 (dicom_img[n] = n번째 프레임)
-
             k = self.didx[i]
+            print(f'[{k}]파일')
+
             if bas==True: dicom_img = np.load('DENOISED/'+str(k+1)+'(b).npy')
             else: dicom_img = np.load('DENOISED/'+str(k+1)+'(h).npy')
 
@@ -66,59 +67,81 @@ class excel:
             ##옵티컬플로우 or 밝기값
             if opt == True: mat_i, mat_x = dicom_optflow().do_optflow(dicom_img, all_frame, preprocess)
             else: mat_i, mat_x = dicom_intense().do_intense(dicom_img, all_frame, preprocess)
-
+            #print('Intensity 게산완료')
             #dicom_img = dicom_intense().proc_data(dicom_img)
 
             ##보간사용/미사용
             plt.plot(mat_i, mat_x, color='black')
-            colors = ['lime', 'yellow', 'magenta', 'gold', 'grey']
 
             if interpolate == False: plt.plot(mat_i, mat_x, color='lime')
             else:
-                for i in range(cycle):
-                    interp_i, mat_x1 = interp().peak_interpolation(mat_i, mat_x) #1차 보간
-                    TFCS, TFCE, CCFCS, CCFCE = analysis().get_TFC_CCFC(np.array(interp_i), np.array(mat_x1), isgmm=True, draw=True) #클러스터링(n=2)기반 극대극소를 발견하는 TFC
+                interp_i, mat_x1 = interp().peak_interpolation(mat_i, mat_x) #1차 보간
+                #print('프레임 계산 시작')
+                TFCS, TFCE, CCFCS, CCFCE = analysis().get_TFC_CCFC(np.array(interp_i), np.array(mat_x1), isgmm=False, test=True) #클러스터링(n=2)기반 극대극소를 발견하는 TFC
+                #print('프레임 계산 종료')
+
 
             ##############################결과출력
-            seires = ''
+            series = ''
             if bas==True: series = 'b'
             else: series='h'
-            rtfc_s, rtfc_e = self.df['TFC_start_'+series][k].astype(int), self.df['TFC_end_'+series][k].astype(int)
-            rccfc_s, rccfc_e = self.df['CCFC_start_'+series][k].astype(int), self.df['CCFC_end_'+series][k].astype(int)
+            ####실제 TFC, CCFC 저장
+            tfcs_real, tfce_real = self.drop_df['TFC_start_'+series][k].astype(int), self.drop_df['TFC_end_'+series][k].astype(int)
+            ccfcs_real, ccfce_real = self.drop_df['CCFC_start_'+series][k].astype(int), self.drop_df['CCFC_end_'+series][k].astype(int)
+            rtfcs.append(tfcs_real); rtfce.append(tfce_real); rccfcs.append(ccfcs_real); rccfce.append(ccfce_real)
 
-            tfcs_pred.append(TFCS); tfce_pred.append(TFCE); ccfcs_pred.append(CCFCS); ccfce_pred.append(CCFCE)
-            rtfcs.append(rtfc_s); rtfce.append(rtfc_e); rccfcs.append(rccfc_s); rccfce.append(rccfc_e)
+            #1개의 파일에 대한 [[TFCS], [TFCE], [CCFCS], [CCFCE]]가 반환되므로.. n차항별로 분리해야한다.
+            #[ [2차TFCS, 3차TFCS ...], [2차TFCE, 3차TFCE, ...], ...] 이기 때문에.
 
-            #########################
-
-            print(f'[{k+1}] TFC: [{rtfc_s}, {rtfc_e}], predict: [{TFCS}, {TFCE}]')
-            print(f'CCFC: [{rccfc_s}, {rccfc_e}], predict: [{CCFCS}, {CCFCE}]')
-            plt.title(f'[{int(k+1)}] TFC: [{rtfcs}, {rtfce}], predict: [{TFCS}, {TFCE}]\n CCFC: [{rccfcs}, {rccfce}], predict: [{CCFCS}, {CCFCE}]', fontsize=15)
+            for j in range(5): 
+                results[j][0].append(TFCS[j]) # [ [j차 피팅], [j+1차 피팅], ..] -> [j차피팅]안에는 [ [j차 TFCS], [j차 TFCE], [j차 CCFCS], [j차 CCFCE] ] 처럼 생김
+                results[j][1].append(TFCE[j])
+                results[j][2].append(CCFCS[j])
+                results[j][3].append(CCFCE[j])
             #####################################
 
             ##엑셀속 frame count 표시
-            plt.axvspan(self.drop_df['TFC_start_'+series][k].astype(int), self.drop_df['TFC_end_'+series][k].astype(int), facecolor='red')
-            plt.axvspan(self.drop_df['CCFC_start_'+series][k].astype(int), self.drop_df['CCFC_end_'+series][k].astype(int), facecolor='blue')
+            #plt.axvspan(tfcs_real, tfce_real, facecolor='red')
+            #plt.axvspan(ccfcs_real, ccfce_real, facecolor='blue')
             if save == True: plt.savefig('all_plot/'+f'{k+1}.png')
             plt.clf()
+            #print('파일작업 종료')
             
         ###오차 평가
-        tfcs_rms = sqrt(mean_squared_error(rtfcs, tfcs_pred))
-        tfce_rms = sqrt(mean_squared_error(rtfce, tfce_pred))
-        print(f'tfcs score={tfcs_rms}, tfce score={tfce_rms}')
+        print('RMSE 평가 시작')
+        min_tfcs, min_tfce, min_ccfcs, min_ccfce = [1000,0], [1000,0], [1000,0], [1000,0]
+        for i in range(results):
+            res = results[i] #res = [i차 피팅] = [ [i차 TFCS], [i차 TFCE], [i차 CCFCS], [i차 CCFCE] ]
+            tfcs_rmse = sqrt(mean_squared_error(rtfcs, res[0])) #res[0] = n차 피팅 TFCS
+            if min_tfcs[0] > tfcs_rmse:
+                min_tfcs[0] = tfcs_rmse; min_tfcs[1] = i
 
-        ccfcs_rms = sqrt(mean_squared_error(rccfcs, ccfcs_pred))
-        ccfce_rms = sqrt(mean_squared_error(rccfce, ccfce_pred))
-        print(f'ccfcs score={ccfcs_rms}, ccfce score={ccfce_rms}')
+            tfce_rmse = sqrt(mean_squared_error(rtfce, res[1]))
+            if min_tfce[0] > tfce_rmse:
+                min_tfce[0] = tfce_rmse; min_tfce[1] = i
 
-        print('---------------------------------------------------------')
+            ccfcs_rmse = sqrt(mean_squared_error(rccfcs, res[2]))
+            if min_ccfcs[0] > ccfcs_rmse:
+                min_ccfcs[0] = ccfcs_rmse; min_ccfcs[1] = i
+
+            ccfce_rmse = sqrt(mean_squared_error(rccfce, res[3]))
+            if min_ccfce[0] > ccfce_rmse:
+                min_ccfce[0] = ccfce_rmse; min_ccfce[1] = i
+            
+        print(f'TFCS RMSE = {min_tfcs[0]} ({min_tfcs[1]}차함수)')
+        print(f'TFCE RMSE = {min_tfce[0]} ({min_tfce[1]}차함수)')
+        print(f'CCFCS RMSE = {min_ccfcs[0]} ({min_ccfcs[1]}차함수)')
+        print(f'CCFCE RMSE = {min_ccfce[0]} ({min_ccfce[1]}차함수)')
+
+        print('----------RMSE 평가 종료')
 
         #################
     ####################################################
 
 
 
-    def show_plot(self, pat_num, bas=True, preprocess=False, interpolate=False, cycle=1, opt=False):  
+
+    def show_plot(self, pat_num, bas=True, preprocess=False, interpolate=False, opt=False):  #특정파일에 대한 하강벡터수 확인
         pat_num-=1
         path = self.load_excel(pat_num)
         dicom_data = pydicom.dcmread(path) #force=True 인자를 넣으면 인식안되는 dicom 읽을수 있음
@@ -131,18 +154,17 @@ class excel:
         ##옵티컬플로우 or 밝기값
         if opt == True: mat_i, mat_x = dicom_optflow().do_optflow(dicom_img, all_frame, preprocess)
         else: mat_i, mat_x = dicom_intense().do_intense(dicom_img, all_frame, preprocess)
+
         
         ##보간사용/미사용
         k = pat_num; interp_i=[]
         plt.plot(mat_i, mat_x, color='black')
         if interpolate == False: plt.plot(mat_i, mat_x, color='black')
         else:
-            for i in range(cycle):
-                interp_i, mat_x1 = interp().peak_interpolation(mat_i, mat_x) #1차 보간
-                TFCS, TFCE, CCFCS, CCFCE = analysis().get_TFC_CCFC(np.array(interp_i), np.array(mat_x1), isgmm=True, draw=True) #클러스터링(n=2)기반 극대극소를 발견하는 TFC
-                #TFCS, TFCE, CCFCS, CCFCE = analysis().get_TFC_CCFC(np.array(mat_i), np.array(mat_x), isgmm=True, draw=True) #클러스터링(n=2)기반 극대극소를 발견하는 TFC
+            interp_i, mat_x1 = interp().peak_interpolation(mat_i, mat_x) #1차 보간
+            TFCS, TFCE, CCFCS, CCFCE = analysis().get_TFC_CCFC(np.array(interp_i), np.array(mat_x1), isgmm=False) #클러스터링(n=2)기반 극대극소를 발견하는 TFC
 
-        seires = ''
+        series = ''
         if bas==True: series = 'b'
         else: series='h'
         rtfcs, rtfce = self.df['TFC_start_'+series][k].astype(int), self.df['TFC_end_'+series][k].astype(int)
@@ -153,7 +175,7 @@ class excel:
         plt.title(f'[{int(k+1)}] TFC: [{rtfcs}, {rtfce}], predict: [{TFCS}, {TFCE}]\n CCFC: [{rccfcs}, {rccfce}], predict: [{CCFCS}, {CCFCE}]', fontsize=15)
 
         ##엑셀속 frame count 표시
-        plt.axvspan(self.df['TFC_start_'+series][k].astype(int), self.df['TFC_end_'+series][k].astype(int), facecolor='red')
-        plt.axvspan(self.df['CCFC_start_'+series][k].astype(int), self.df['CCFC_end_'+series][k].astype(int), facecolor='blue')
+        plt.axvspan(self.df['TFC_start_'+series][k].astype(int), self.df['TFC_end_'+series][k].astype(int), facecolor='grey', alpha=0.4, zorder=1)
+        plt.axvspan(self.df['CCFC_start_'+series][k].astype(int), self.df['CCFC_end_'+series][k].astype(int), facecolor='grey', alpha=0.4, zorder=1)
         plt.show()
     #######################################################
